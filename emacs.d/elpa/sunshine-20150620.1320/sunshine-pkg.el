b2e4ece2feaaedf5b16b0c2c49c66b8aa01092bb
Ashley Gillman <ashley.gillman@my.jcu.edu.au>
2015-07-30 16:11:52 +1000

    initial emacs config, mostly taken from Aaron Bieber. Has some extra stuff regarding org-mode for bilbliographies.

---
+++ emacs.d/elpa/sunshine-20150620.1320/sunshine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sunshine" "20150620.1320" "Provide weather and forecast information." '((cl-lib "0.5")) :url "https://github.com/aaronbieber/sunshine.el" :keywords '("tools" "weather"))
---
+++ emacs.d/lisp/init-maps.el
@@ -0,0 +1,19 @@
+;;; Provide global maps that aren't specific to any mode or package.
+
+(define-key global-map (kbd "C-c t w") (lambda ()
+                                         (interactive)
+                                         (find-file-other-window "~/Dropbox/org/work.org")))
+(define-key global-map (kbd "C-c t h") (lambda ()
+                                         (interactive)
+                                         (find-file-other-window "~/Dropbox/org/home.org")))
+(define-key global-map (kbd "C-c t a") 'org-agenda-list)
+(define-key global-map (kbd "C-x C-q") 'kill-emacs)
+(define-key global-map (kbd "C-c C-u") 'insert-char) ;; "u" for Unicode, get it?
+(define-key global-map (kbd "C-c l")   'dictionary-lookup-definition)
+(define-key global-map (kbd "C-c d f") 'find-name-dired)
+
+;; C-v is "visual block" in normal mode, but use it for "paste" in insert mode.
+(when (equal system-type 'darwin)
+  (evil-define-key 'insert global-map (kbd "C-v") 'yank))
+
+(provide 'init-maps)
