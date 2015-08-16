39cfb71fa7dc41d1e537457c9710b1997599d5d2
Ashley Gillman <ashley.gillman@my.jcu.edu.au>
2015-08-16 19:53:16 +1000

    Cleanup :shower:

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -291,7 +291,7 @@
  '(org-mobile-inbox-for-pull "~/Dropbox/org/mobile/capture.org")
  '(package-selected-packages
    (quote
-    (ox-bibtex htmlize flymake-coffee wakatime-mode zenburn-theme yasnippet yaml-mode wgrep-ag web-mode w3m twittering-mode sunshine sublime-themes powerline-evil php-extras mmm-mode markdown-mode magit hyde highlight-symbol helm-projectile guide-key gtags fullframe flycheck exec-path-from-shell evil-surround evil-leader evil-jumper evil-indent-textobject emmet-mode diminish dictionary circe avy auto-complete ag)))
+    (multi-term ox-bibtex htmlize flymake-coffee wakatime-mode zenburn-theme yasnippet yaml-mode wgrep-ag web-mode w3m twittering-mode sunshine sublime-themes powerline-evil php-extras mmm-mode markdown-mode magit hyde highlight-symbol helm-projectile guide-key gtags fullframe flycheck exec-path-from-shell evil-surround evil-leader evil-jumper evil-indent-textobject emmet-mode diminish dictionary circe avy auto-complete ag)))
  '(python-indent-offset 2)
  '(safe-local-variable-values (quote ((no-byte-compile t) (require-final-newline))))
  '(scss-compile-at-save nil)
--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -42,37 +42,6 @@
   (define-key global-map (kbd "C-c a") 'org-agenda)
   (setq org-log-done t)
 
-  ;; https://tincman.wordpress.com/2011/01/04/research-paper-management-with-emacs-org-mode-and-reftex/
-  (defun org-mode-reftex-setup ()
-    (load-library "reftex")
-    (and (buffer-file-name) (file-exists-p (buffer-file-name))
-         (progn
-           ;enable auto-revert-mode to update reftex when bibtex file changes on disk
-           (global-auto-revert-mode t)
-           ;(reftex-parse-all)
-           ;add a custom reftex cite format to insert links
-           (reftex-set-cite-format
-            '((?b . "[[bib:%l][%l-bib]]")
-              (?n . "[[notes:%l][%l-notes]]")
-              (?p . "[[papers:%l][%l-paper]]")
-              (?t . "%t")
-              (?a . "\\autocite{%l}")
-              (?h . "*** %t\n:PROPERTIES:\n:Custom_ID: %l\n:END:\n\\cite{%l}\n****     :noexport:\n[[papers:%l][%l-paper]] [[bib:%l][BibTeX]]")))))
-    (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
-    (define-key org-mode-map (kbd "C-c (") 'org-mode-reftex-search))
-
-  (defun org-mode-reftex-search ()
-    ;;jump to the notes for the paper pointed to at from reftex search
-    (interactive)
-    (org-open-link-from-string (format "[[notes:%s]]" (first (reftex-citation t)))))
-
-  (setq org-link-abbrev-alist
-      '(("bib" . "./bibliography.bib::%s")
-	("notes" . "./bibliography.org::#%s")
-	("papers" . "./papers/%s.pdf")))
-
-  (add-hook 'org-mode-hook 'org-mode-reftex-setup)
-
   ;; https://www-public.tem-tsp.eu/~berger_o/weblog/2012/03/23/how-to-manage-and-export-bibliographic-notesrefs-in-org-mode/
   (defun my-rtcite-export-handler (path desc format)
     (message "my-rtcite-export-handler is called : path = %s, desc = %s, format = %s" path desc format)
@@ -106,5 +75,36 @@
 ;'(reftex-use-external-file-finders t)
 ;(require 'ox-bibtex)
 (setq reftex-default-bibliography '("./bibliography.bib"))
+
+;; https://tincman.wordpress.com/2011/01/04/research-paper-management-with-emacs-org-mode-and-reftex/
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name) (file-exists-p (buffer-file-name))
+       (progn
+         ;enable auto-revert-mode to update reftex when bibtex file changes on disk
+         (global-auto-revert-mode t)
+         ;(reftex-parse-all)
+         ;add a custom reftex cite format to insert links
+         (reftex-set-cite-format
+          '((?b . "[[bib:%l][%l-bib]]")
+            (?n . "[[notes:%l][%l-notes]]")
+            (?p . "[[papers:%l][%l-paper]]")
+            (?t . "\\textcite{%l}")
+            (?a . "\\autocite{%l}")
+            (?h . "*** %t\n:PROPERTIES:\n:Custom_ID: %l\n:END:\n\\cite{%l}\n****     :noexport:\n[[papers:%l][%l-paper]] [[bib:%l][BibTeX]]")))))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
+  (define-key org-mode-map (kbd "C-c (") 'org-mode-reftex-search))
+
+(defun org-mode-reftex-search ()
+  ;;jump to the notes for the paper pointed to at from reftex search
+  (interactive)
+  (org-open-link-from-string (format "[[notes:%s]]" (first (reftex-citation t)))))
+
+(setq org-link-abbrev-alist
+    '(("bib" . "./bibliography.bib::%s")
+      ("notes" . "./bibliography.org::#%s")
+      ("papers" . "./papers/%s.pdf")))
+
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
 
 (provide 'init-org)
